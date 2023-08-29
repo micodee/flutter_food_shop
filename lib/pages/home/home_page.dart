e3203cac3537d1d93b707a29ae0834533ddad6c4
micodee <martomhidayat>
2023-08-29 12:47:34 +0700

    add nav bottom

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_shop/pages/home/main_food_page.dart';
+import 'package:flutter_food_shop/utils/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +10,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
 
   List pages=[
     MainFoodPage(),
@@ -29,10 +32,47 @@ class _HomePageState extends State<HomePage> {
     ),
   ];
 
+  void onTapNav(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MainFoodPage(),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+
+        selectedItemColor: AppColors.mainColor,
+        unselectedItemColor: Colors.amberAccent,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex, // for color buttton
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+
+        onTap: onTapNav,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined,),
+            label: "home"
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.archive,),
+            label: "history"
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart,),
+            label: "home"
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person,),
+            label: "home"
+            ),
+        ],
+      ),
     );
   }
+
 }
